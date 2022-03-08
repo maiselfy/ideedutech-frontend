17789f9751178d975798d87820c83989e9816af7
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-03-07 22:02:04 -0300

    feat: admin profile popover

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -1,27 +1,74 @@
-import { Avatar, Box, Flex, Image, Spacer, Text } from '@chakra-ui/react';
-import React from 'react';
+import {
+  Avatar,
+  Box,
+  Flex,
+  Icon,
+  Image,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Spacer,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { RiLogoutCircleLine } from 'react-icons/ri';
 
 const AdminSidebar: React.FC = () => {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <Flex background="gray.50" padding="4" justify="center" _hover="gray.200">
-      <Image src="https://user-images.githubusercontent.com/49327985/154309877-cd022fe2-feb8-4193-b61a-baea24104eec.svg" />
+    <Flex background="gray.50" justify="center">
+      <Image
+        padding="4"
+        src="https://user-images.githubusercontent.com/49327985/154309877-cd022fe2-feb8-4193-b61a-baea24104eec.svg"
+      />
       <Spacer />
-      <Flex>
-        <Flex
-          flexDirection="column"
-          justify="center"
-          marginRight="4"
-          textAlign="right"
-        >
-          <Text color="text" fontWeight="medium">
-            Marcos Gênesis
-          </Text>
-          <Text color="gray.500" fontSize="xx-small">
-            ADMINISTRADOR
-          </Text>
-        </Flex>
-        <Avatar />
-      </Flex>
+
+      <Popover>
+        <PopoverTrigger>
+          <Flex cursor="pointer" _hover={{ bgColor: 'gray.100' }} padding="4">
+            <Flex
+              flexDirection="column"
+              justify="center"
+              marginRight="4"
+              textAlign="right"
+            >
+              <Text color="text" fontWeight="medium">
+                Marcos Gênesis
+              </Text>
+              <Text color="gray.500" fontSize="xx-small">
+                ADMINISTRADOR
+              </Text>
+            </Flex>
+            <Avatar />
+          </Flex>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader>Opções</PopoverHeader>
+          <PopoverBody>
+            <VStack spacing="4">
+              <Flex
+                w="100%"
+                borderRadius="4"
+                p="2"
+                _hover={{ bgColor: 'gray.100'}}
+                justifyContent="center"
+                alignItems="center"
+                cursor="pointer"
+              >
+                <Icon as={RiLogoutCircleLine} marginRight="4" />
+                <Text>Sair</Text>
+              </Flex>
+            </VStack>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
     </Flex>
   );
 };
