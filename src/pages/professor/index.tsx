dcc3507199e11e593b7c4a3794928c2662e24eda
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-03-08 15:13:15 -0300

    feat: task listing

--- src/pages/professor/index.tsx
+++ src/pages/professor/index.tsx
@@ -1,11 +1,120 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Img,
+  SimpleGrid,
+  Spacer,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import React from 'react';
 import ProfessorSideBar from '@/components/professor/ProfessorSideBar';
+import {
+  RiBookLine,
+  RiCalendarLine,
+  RiMore2Line,
+  RiTeamLine,
+} from 'react-icons/ri';
 
 const ProfessorDashboard: React.FC = () => {
   return (
-    <Flex>
+    <Flex flexDirection="column">
       <ProfessorSideBar />
+      <SimpleGrid columns={3} spacing={16}>
+        <Flex flex="1" flexDirection="column" p="4">
+          <Text fontSize="3xl" fontWeight="bold">
+            Atividades
+          </Text>
+          <VStack spacing="4">
+            {[1, 2, 3, 4, 5, 6].map((x) => (
+              <Flex
+                bgColor="gray.50"
+                w="100%"
+                flexDirection="column"
+                p="4"
+                borderRadius="lg"
+              >
+                <Flex justifyContent="center" align="center">
+                  <Box display="flex" alignItems="center" justifyContent="center" w="12" h="12" bg="green.100" mr="2" borderRadius="md">
+                    <Img
+                      w="8"
+                      h="8"
+                      src="https://user-images.githubusercontent.com/49327985/157252598-251b1303-680c-456f-9ff1-6dfe380750ce.png"
+                    />
+                  </Box>
+                  <Box>
+                    <Text fontSize="x-small">APRESENTAÇÃO</Text>
+                    <Text fontWeight="medium">Conflito na síria</Text>
+                  </Box>
+                  <Spacer />
+                  <IconButton variant="ghost" icon={<RiMore2Line />} />
+                </Flex>
+                <Flex paddingTop="4" paddingBottom="4">
+                  <Text>
+                    Apresentação sobre os conflitos da síria, com no mínimo 10
+                    slides, em grupo de 3 alunos.
+                  </Text>
+                </Flex>
+                <Box>
+                  <HStack>
+                    <Flex justifyContent="center" align="center">
+                      <Icon as={RiBookLine} color="green.600" mr="2" />
+                      <Text>Geografia</Text>
+                    </Flex>
+                    <Flex justifyContent="center" align="center">
+                      <Icon as={RiTeamLine} color="green.600" mr="2" />
+                      <Text>3 ano C</Text>
+                    </Flex>
+                  </HStack>
+                  <Flex justifyContent="space-between" mt="2">
+                    <Flex align="center" justifyContent="center">
+                      <AvatarGroup size="sm" max={2} mr="2">
+                        <Avatar
+                          name="Ryan Florence"
+                          src="https://bit.ly/ryan-florence"
+                        />
+                        <Avatar
+                          name="Segun Adebayo"
+                          src="https://bit.ly/sage-adebayo"
+                        />
+                        <Avatar
+                          name="Kent Dodds"
+                          src="https://bit.ly/kent-c-dodds"
+                        />
+                        <Avatar
+                          name="Prosper Otemuyiwa"
+                          src="https://bit.ly/prosper-baba"
+                        />
+                        <Avatar
+                          name="Christian Nwamba"
+                          src="https://bit.ly/code-beast"
+                        />
+                      </AvatarGroup>
+                      <Text fontSize="sm">12/45 concluídos</Text>
+                    </Flex>
+                    <Button leftIcon={<RiCalendarLine />}>
+                      <Text fontWeight="normal">Não Iniciada</Text>
+                    </Button>
+                  </Flex>
+                </Box>
+              </Flex>
+            ))}
+          </VStack>
+        </Flex>
+        <Flex flex="1">
+          <Text fontSize="3xl" fontWeight="bold">
+            Avaliações
+          </Text>
+        </Flex>
+        <Flex bgColor="gray.50" height="100vh"></Flex>
+      </SimpleGrid>
     </Flex>
   );
 };
